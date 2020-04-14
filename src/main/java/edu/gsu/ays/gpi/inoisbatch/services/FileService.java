3efcde03e542a96f55e89e9124b2899aefe11f69
chad.mckee <chad.mckee@slalom.com>
2020-04-14 12:27:47 -0400

    basic decryption

--- src/main/java/edu/gsu/ays/gpi/inoisbatch/services/FileService.java
+++ src/main/java/edu/gsu/ays/gpi/inoisbatch/services/FileService.java
@@ -2,12 +2,15 @@ package edu.gsu.ays.gpi.inoisbatch.services;
 
 
 import java.io.*;
+import java.time.temporal.TemporalAmount;
 import java.util.UUID;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
-
+import com.macasaet.fernet.*;
+import java.time.Duration;
+import java.time.Instant;
 import edu.gsu.ays.gpi.inoisbatch.tasks.ProcessEntityData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,19 +26,43 @@ public class FileService {
     private static final String accountName = System.getenv("APPSETTING_StorageAccountName");
     private static final String containerName = System.getenv("APPSETTING_BlobContainerIngest");
 
+
+
     public static void retrieveBlob(String file) throws IOException {
+
+        final Key key = new Key("wIFnWSvZv-3cuPuz19WKkqIudlwCYnynMI1VyjJg0dE=");
 
         final String connectionString = buildConnectionString();
         //final String fileName = UUID.randomUUID().toString();
+        String blobContents;
 
         BlobClient blobClient = buildBlobClient(connectionString, file);
         //InputStream inputStream = new BufferedInputStream(file.getInputStream());
         //blobClient.upload(inputStream, file.getBytes().length);
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()){
             blobClient.download(outputStream);
-            log.info("File downloaded: ");
+            log.info("File downloaded.");
             log.info(outputStream.toString());
+            blobContents = outputStream.toString();
         }
+        catch (Exception ex){
+            log.error(ex.getMessage());
+            throw ex;
+        }
+        final Token token = Token.fromString(blobContents);
+
+/*        final Validator<String> validator = new StringValidator() {
+        };*/
+
+        final Validator < String > validator = new StringValidator() {
+            public TemporalAmount getTimeToLive() {
+                return Duration.ofSeconds(Instant.MAX.getEpochSecond());
+            }
+        };
+
+
+        final String payload = token.validateAndDecrypt(key, validator);
+        log.info("Decrypted file: \n " + payload);
 
 
         //log.info("File downloaded: " + fileName);
