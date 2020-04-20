23f70c2523443b25aafda10c47b5b3d7218ad968
chad.mckee <chad.mckee@slalom.com>
2020-04-20 12:24:13 -0400

    file status constants, custom exceptions

---
+++ src/main/java/edu/gsu/ays/gpi/inoisbatch/exceptions/InvalidFileFormatError.java
@@ -0,0 +1,7 @@
+package edu.gsu.ays.gpi.inoisbatch.exceptions;
+
+public class InvalidFileFormatError extends RuntimeException {
+    public InvalidFileFormatError(String message) {
+        super(message);
+    }
+}
